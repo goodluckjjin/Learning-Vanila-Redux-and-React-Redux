d9884b0c67e794f89659ddb5568aa406255341b0
정지은 <jeongjieun@jeongjieun-ui-MacBookPro.local>
2022-03-23 01:06:57 +0900

    1.2 Action

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
+import {createStore} from 'redux';
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const countModifier = (count = 0, action) => {
+  console.log("1", action);
+  if(action.type ==="ADD") {
+    return count +1;
+  } else if(action.type === "MINUS") {
+    return count-1;
+  } else {
+    return count;
+  }
+  // return count;
+};
 
-let count = 0;
-// number.innerText = count;
-
-const updateCount = () => {
-  number.innerText = count;
-  
-}
-
-const handleAdd = () => {
-  count = count+1;
-  updateCount();
-}
-
-const handleMinus = () => {
-  count = count-1;
-  updateCount();
-
-}
+const countStore = createStore(countModifier);
 
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+//action 보내기 위해서는 countStore.dispatch 이용
+countStore.dispatch({type:"ADD"});
+countStore.dispatch({type:"ADD"});
+countStore.dispatch({type:"ADD"});
+countStore.dispatch({type:"MINUS"});
+
+
+console.log(countStore.getState());
