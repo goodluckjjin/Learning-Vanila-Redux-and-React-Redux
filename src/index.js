e00d4c0a7d9b7d3aae44acda5d7bfdfc41e15b62
jeongjieun <assd3327@gmail.com>
2022-03-23 17:42:38 +0900

    1.3 Subscriptions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
+//reducer : data 변경
 const countModifier = (count = 0, action) => {
   console.log("1", action);
   if(action.type ==="ADD") {
@@ -13,17 +16,38 @@ const countModifier = (count = 0, action) => {
   } else {
     return count;
   }
-  // return count;
 };
 
 const countStore = createStore(countModifier);
 
 
 //action 보내기 위해서는 countStore.dispatch 이용
-countStore.dispatch({type:"ADD"});
-countStore.dispatch({type:"ADD"});
-countStore.dispatch({type:"ADD"});
-countStore.dispatch({type:"MINUS"});
+// countStore.dispatch({type:"ADD"});
+// countStore.dispatch({type:"ADD"});
+// countStore.dispatch({type:"ADD"});
+// countStore.dispatch({type:"MINUS"});
 
 
-console.log(countStore.getState());
+const onChange = () => {
+  console.log("there was a change on the store");
+  console.log(countStore.getState());
+  number.innerText = countStore.getState();
+}
+
+countStore.subscribe(onChange);
+
+const handleAdd = ()=> {
+  countStore.dispatch({type:"ADD"});
+}
+
+const handleMinus = ()=> {
+  countStore.dispatch({type:"MINUS"});
+}
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
+
+console.log("countStore", countStore);
+// dispatch : action 행사 = store와 커뮤니케이션
+// getState : state값 조회
+// subscribe : action으로 인한 변화 감지
