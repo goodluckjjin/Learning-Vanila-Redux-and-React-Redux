3c703e6715323a9f720ded5c4dd08c28820bba8c
jeongjieun <assd3327@gmail.com>
2022-03-28 18:22:36 +0900

    setting todolist

--- src/index.js
+++ src/index.js
@@ -1,56 +1,21 @@
-import {createStore} from 'redux';
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-
-number.innerText = 0;
-
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-//reducer : data 변경되는 곳
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return count +1;
-    case MINUS:
-      return count -1;
-    default:
-      return count;
-  }
+const createToDo = todo => {
+  const li = document.createElement("li");
+  ul.appendChild(li);
 };
 
-const countStore = createStore(countModifier);
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  createToDo(toDo);
+};
 
+form.addEventListener("submit". onSubmit);
 
-//action 보내기 위해서는 countStore.dispatch 이용
-// countStore.dispatch({type:"ADD"});
-// countStore.dispatch({type:"ADD"});
-// countStore.dispatch({type:"ADD"});
-// countStore.dispatch({type:"MINUS"});
-
-
-const onChange = () => {
-  console.log("there was a change on the store");
-  console.log(countStore.getState());
-  number.innerText = countStore.getState();
-}
-
-countStore.subscribe(onChange);
-
-const handleAdd = ()=> {
-  countStore.dispatch({type: ADD});
-}
-
-const handleMinus = ()=> {
-  countStore.dispatch({type: MINUS});
-}
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
-
-console.log("countStore", countStore);
 // dispatch : reducer를 불러서 action 행사 = store와 커뮤니케이션
 // action : reducer와 커뮤니케이션
 // getState : state값 조회
